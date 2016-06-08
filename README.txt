797107dd6816126d7d47284f55548663821f2f1f
yalk <ankur2893@gmail.com>
2016-06-08 02:16:59 -0400

    better o/p, less chance of google ban

--- README.txt
+++ README.txt
@@ -1,4 +1,23 @@
-enter search terms for songs, preferably in the format <artist> - <song title> in songs.txt.
-an example has been provide.
-run script.
-songs will be in the DownloadedSongs folder
+==========================================================
+Download  songs by providing correct artist and song name
+==========================================================
+
+=============
+Introduction:
+=============
+Download your favorite songs by just entering the artist and song title in this program. You can also give it a list of artists and the songs. Wait for sometime and you will have all those songs on your system :)
+
+====================
+Steps to run script
+====================
+1. Enter search terms for songs, preferably in the format <artist> - <song title> in songs.txt
+2. Run script
+3. Songs will be in the DownloadedSongs folder
+4. Song name and corresponding YouTube URL is mentioned in songLinks.txt
+
+=============
+Dependencies
+=============
+1. python-2.7
+2. youtube-dl
+3. python packages used in the script
--- SongsDownloader.py
+++ SongsDownloader.py
@@ -6,24 +6,37 @@ import subprocess
 songsfile= open('songs.txt','r')
 
 with open('songs.txt', 'r') as songsfile:
-	    mySongsList= songsfile.read().splitlines()
-mysonglistURL=[]
+    mySongsList= songsfile.read().splitlines()
+    totalSongs=len(mySongsList)
+i=1
+songsListMapper=[]
+
+print "YouTube Song Downloader v1.2"
+
 for line in mySongsList:
+
     query= str(line+r" site:youtube.com")
-    print query,
+    print "\n\nTitle: "+line
+
     for url in search(query, num=1, stop=1):
-        mysonglistURL.append(url)
-        print url
 
-print "\n--++==List of URLs==++--\n"
-for line in mysonglistURL:
-    print str(line)
+        print "URL: "+url+"\n\nDownloading..."
+        
+        try:
+            print subprocess.check_output(["youtube-dl", "-o", "./DownloadedSongs/%(title)s.%(ext)s", "-x", "--audio-format", "mp3", "--yes-playlist", "--audio-quality", "0", url])
 
-for line in mysonglistURL:
-    print "\nDownloading: "+line
-    process_call= "youtube-dl -x --audio-format mp3 --yes-playlist --audio-quality 0 "+line
-    try:
-        print subprocess.check_output(["youtube-dl", "-o", "./DownloadedSongs/%(title)s.%(ext)s", "-x", "--audio-format", "mp3", "--yes-playlist", "--audio-quality", "0", line])
-    except subprocess.CalledProcessError, e:
-        print "Ping stdout output:\n", e.output
-    print "\nFinished Download: "+line
+        except subprocess.CalledProcessError, e:
+            print "Ping stdout output:\n", e.output
+
+        print "Download Complete!"
+        currentMap=[line,url]
+        songsListMapper.append(currentMap)
+    i=i+1
+
+SongURLListFile= open("songLinks.txt","wb")
+for line in songsListMapper:
+    SongURLListFile.write(line[0]+","+line[1]+"\n")
+SongURLListFile.close()
+
+SongURLListFile= open("songLinks.txt","r")
+print "\n\n--++==Final URL List==++--\n"+str(SongURLListFile.read())
